01e99061c9fe2a9b012d0ce5ce49757bca698b63
brianrono <brianrono16@gmail.com>
2023-03-13 22:43:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random); 
+
+let num3 = 20;
+let num4 = 5;
+let mod = (num3/num4);
+console.log(mod);
+
+let num5 = 9;
+let num6 = 2;
+let num7 = 20;
+let num8 = 16;
+let num9 = 11;
+let max = Math.max(num5, num6, num7, num8, num9);
+console.log(max);
